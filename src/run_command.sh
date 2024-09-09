8b7cc964279f4a0d047d684952e3784e6af3660d
vkbui <vuongkbui@gmail.com>
2024-09-09 09:42:29 -0700

    updating git ignore

--- src/run_command.sh
+++ src/run_command.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 export PATH=$PATH:/usr/local/bin
+echo "UPDATING AND DEPLOYING PORTFOLIO"
 echo "Current date and time: $(date)"
 /Library/Frameworks/Python.framework/Versions/3.12/bin/python3 /Users/vuongbui/portfolio/src/test.py
 cd /Users/vuongbui/portfolio
-git add . 
-git commit
-git push
-# /usr/local/bin/npm run deploy
+/usr/local/bin/npm run deploy
