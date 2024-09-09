129b17ad98b77cd28c42b7ece29645a39928470f
vkbui <vuongkbui@gmail.com>
2024-09-09 09:49:03 -0700

    Remove run_command.sh and add it to .gitignore

--- src/run_command.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export PATH=$PATH:/usr/local/bin
-echo "UPDATING AND DEPLOYING PORTFOLIO"
-echo "Current date and time: $(date)"
-/Library/Frameworks/Python.framework/Versions/3.12/bin/python3 /Users/vuongbui/portfolio/src/test.py
-cd /Users/vuongbui/portfolio
-/usr/local/bin/npm run deploy
